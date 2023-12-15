48c77902fdf415946bbda8e12cc41fdb91508446
CodeNachos <rafael.nery14@gmail.com>
2023-12-15 12:02:02 +0100

    werror.h and journal_logs

---
+++ werror.h
@@ -0,0 +1,22 @@
+#ifndef _WERR_H_
+#define _WERR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {
+    SUCCESS = 0,
+    FAILURE = 1,
+    ALLOC_ERROR,
+    FILE_NOT_FOUND,
+    INVALID_ARGUMENTS,
+    PERMISSION_DENIED,
+    INDEX_OUT_OF_RANGE,
+    UNDEFINED_BEHAVIOUR,
+} exit_codes;
+
+#define warning(fmt, ...) fprintf(stderr, "WARNING: %s:%s:%d: %s" fmt, __FILE__, __func__, __LINE__ , ##__VA_ARGS__ )
+#define error(fmt, ...) fprintf(stderr, "ERROR: %s:%s:%d: %s" fmt, __FILE__, __func__, __LINE__ , ##__VA_ARGS__)
+#define raise(code, fmt, ...) do {error(fmt, ##__VA_ARGS__); exit(code);} while(0);
+
+#endif
