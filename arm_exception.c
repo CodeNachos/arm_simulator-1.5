bfc6f6b9af12da8810916ec1c84d177eaf20074f
Maxim Frolov <max.fro2021@gmail.com>
2024-01-09 14:20:14 +0100

    prestructure of interruptions

--- arm_exception.c
+++ arm_exception.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_exception.h"
 #include "arm_constants.h"
@@ -30,7 +30,10 @@ Contact: Guillaume.Huard@imag.fr
 #define Exception_bit_9 (CP15_reg1_EEbit << 9)
 
 int arm_exception(arm_core p, uint8_t exception) {
+
+    // since we do not have FIQ and Imprecise Aborts we use a fixed cpsr mode for all exceptions
     uint32_t cpsr = 0x1d3 | Exception_bit_9;
+
     /* As there is no operating system in our simulator, we handle
      * software interrupts here :
      * - 0x123456 is the end of the simulation
@@ -53,11 +56,37 @@ int arm_exception(arm_core p, uint8_t exception) {
             return 0;
         }
     }
-    /* Aside from SWI, we only support RESET initially */
-    /* Semantics of reset interrupt (ARM manual A2-18) */
-    if (exception == RESET) {
-        arm_write_cpsr(p, cpsr);
-        arm_write_register(p, 15, 0);
+
+    else{
+        uint32_t restore_pc = arm_read_register(p, 15);; // to save pc in R14 before exception
+        uint32_t vect_adr; // to define a fixed vector table address of an exception
+
+        switch (exception){
+        /* ARM manual pages A2-18 to A2-26 */
+        case RESET:
+            arm_write_cpsr(p, cpsr); // enter in interruprions mode
+            arm_write_register(p, 15, 0); // go to interruptions handler
+            break;
+        case INTERRUPT:
+            restore_pc += 4; 
+            vect_adr = 0x00000018;
+            break;
+        case UNDEFINED_INSTRUCTION:
+            vect_adr = 0x00000004;
+            break;
+        default:
+            warning("UNPREDICTABLE (An undefined exception!)\n");
+            return exception;
+            break;
+        }
+
+        // shared exceptions treatement
+        arm_write_register(p, 14, restore_pc); // save pc
+        arm_write_spsr(p, arm_read_cpsr(p)); // save cpsr
+        arm_write_cpsr(p, cpsr); // enter in interruprions mode
+        arm_write_register(p, 15, vect_adr); // go to interruptions handler
+
     }
+
     return exception;
 }
