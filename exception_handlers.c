6f192e587557cc80d5143f3939756520a2dcccc5
Maxim Frolov <max.fro2021@gmail.com>
2024-01-10 14:08:32 +0100

    first change to handlers

--- exception_handlers.c
+++ exception_handlers.c
@@ -1,21 +1,42 @@
 #include "exception_handlers.h"
 
-void reset() {
+
+void reset(arm_core p) {
     ;
 }
 
-void undefined_instruction() {
-    ;
+void undefined_instruction(arm_core p) {
+    uint32_t quit_adr;
+    quit_adr = arm_read_register(p, 14);
+    arm_write_cpsr(p, arm_read_spsr(p)); // restore cpsr
+    arm_write_register(p, 15, quit_adr); // quit interruption
 }
 
-void prefetch_abort() {
-    ;
+void prefetch_abort(arm_core p) {
+    uint32_t quit_adr;
+    quit_adr = arm_read_register(p, 14) - 4;
+    arm_write_cpsr(p, arm_read_spsr(p)); // restore cpsr
+    arm_write_register(p, 15, quit_adr); // quit interruption
 }
 
-void data_abort() {
-    ;
+void data_abort(arm_core p) {
+    uint32_t quit_adr;
+    quit_adr = arm_read_register(p, 14) - 8;
+    arm_write_cpsr(p, arm_read_spsr(p)); // restore cpsr
+    arm_write_register(p, 15, quit_adr); // quit interruption
 }
 
-void fiq() {
-    ;
+void fiq(arm_core p) {
+    uint32_t quit_adr;
+    quit_adr = arm_read_register(p, 14) - 4; // restore pc address
+    arm_write_cpsr(p, arm_read_spsr(p)); // restore cpsr
+    arm_write_register(p, 15, quit_adr); // quit interruption
 }
+
+void irq(arm_core p) {
+    uint32_t quit_adr;
+    quit_adr = arm_read_register(p, 14) - 4; // restore pc address
+    arm_write_cpsr(p, arm_read_spsr(p)); // restore cpsr
+    arm_write_register(p, 15, quit_adr); // quit interruption
+}
+
--- exception_handlers.h
+++ exception_handlers.h
@@ -1,16 +1,18 @@
 #ifndef _EXCPT_HDLR_H_
 #define _EXCPT_HDLR_H_
 
-void reset();
+#include "arm_core.h"
 
-void undefined_instruction();
+void reset(arm_core p);
 
-void prefetch_abort();
+void undefined_instruction(arm_core p);
 
-void data_abort();
+void prefetch_abort(arm_core p);
 
-void irq();
+void data_abort(arm_core p);
 
-void fiq();
+void irq(arm_core p);
+
+void fiq(arm_core p);
 
 #endif
