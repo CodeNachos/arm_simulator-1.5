ff6579fd650d51377d5f4a4cf1a6659237190fb2
CodeNachos <rafael.nery14@gmail.com>
2024-01-11 13:22:08 +0100

    farted

---
+++ Examples/excpt.s
@@ -0,0 +1,20 @@
+.global main
+.text
+main:
+    // populating vector table
+
+    // adding movs pc, r14 to 0x4
+    ldr r0, =data_section
+    ldr r1, [r0]
+    mov r2, #0x04
+    str r1, [r2]
+
+    subs lr, pc, #8
+    // raising undefined instruction
+    ldrsh r3, [fp, #-12]
+
+    swi 0x123456    
+
+.data
+data_section:
+    .word 0xe1b0f00e
--- Examples/handlers.s
+++ Examples/handlers.s
@@ -1,4 +1,20 @@
+.global undf_inst
+.global pfch_abort
+.global data_abort
 .global irq
+.global fiq
+
+undf_inst:
+    movs pc, r14
+
+pfch_abort:
+    subs pc, r14, #4
+
+data_abort:
+    subs pc, r14, #4
 
 irq:
+    subs pc, r14, #4
+
+fiq:
     subs pc, r14, #4
