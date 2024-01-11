f56035a5ca2a938a5781bf42588558dea791ff5c
mascarer <rafael.mascarenhas-dalle-nery@etu.univ-grenoble-alpes.fr>
2024-01-11 10:22:56 +0100

    final commit

--- arm_exception.c
+++ arm_exception.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_exception.h"
 #include "arm_constants.h"
@@ -29,8 +29,13 @@ Contact: Guillaume.Huard@imag.fr
 #define CP15_reg1_EEbit 0
 #define Exception_bit_9 (CP15_reg1_EEbit << 9)
 
+//
+
 int arm_exception(arm_core p, uint8_t exception) {
+
+    // basic cpsr mode, ignores everything
     uint32_t cpsr = 0x1d3 | Exception_bit_9;
+
     /* As there is no operating system in our simulator, we handle
      * software interrupts here :
      * - 0x123456 is the end of the simulation
@@ -53,11 +58,73 @@ int arm_exception(arm_core p, uint8_t exception) {
             return 0;
         }
     }
-    /* Aside from SWI, we only support RESET initially */
-    /* Semantics of reset interrupt (ARM manual A2-18) */
-    if (exception == RESET) {
-        arm_write_cpsr(p, cpsr);
-        arm_write_register(p, 15, 0);
+
+    else{
+        uint32_t restore_pc;
+        
+        switch (exception){
+        /* ARM manual pages A2-18 to A2-26 */
+        case RESET:
+            arm_write_cpsr(p, cpsr); // enter in interruptions mode
+            arm_write_register(p, 15, 0); // reset pc
+            break;
+
+        case FAST_INTERRUPT:
+            restore_pc = arm_read_register(p, 15);
+            restore_pc += 4;
+            arm_write_register(p, 14, restore_pc); // save pc
+            arm_write_spsr(p, arm_read_cpsr(p)); // save cpsr
+            arm_write_cpsr(p, cpsr); // enter in interruptions mode  
+            arm_write_register(p, 15, 0x0000001C); // go to interruptions handler
+            break;
+
+        case INTERRUPT:
+            cpsr = clr_bit(cpsr, 6); // do not disable FIQ
+            restore_pc = arm_read_register(p, 15);
+            restore_pc += 4; 
+            arm_write_register(p, 14, restore_pc); // save pc
+            arm_write_spsr(p, arm_read_cpsr(p)); // save cpsr
+            arm_write_cpsr(p, cpsr); // enter in interruptions mode
+            cpsr = set_bit(cpsr, 6); // restore cpsr config back 
+            arm_write_register(p, 15, 0x00000018); // go to interruptions handler
+            break;
+
+        case UNDEFINED_INSTRUCTION:
+            cpsr = clr_bit(cpsr, 6); // do not disable FIQ
+            restore_pc = arm_read_register(p, 15);
+            arm_write_register(p, 14, restore_pc); // save pc
+            arm_write_spsr(p, arm_read_cpsr(p)); // save cpsr
+            arm_write_cpsr(p, cpsr); // enter in interruptions mode 
+            cpsr = set_bit(cpsr, 6); // restore cpsr config back 
+            arm_write_register(p, 15, 0x00000004); // go to interruptions handler
+            break;
+        
+        case PREFETCH_ABORT:
+            cpsr = clr_bit(cpsr, 6); // do not disable FIQ
+            restore_pc = arm_read_register(p, 15);
+            restore_pc += 4;
+            arm_write_register(p, 14, restore_pc); // save pc
+            arm_write_spsr(p, arm_read_cpsr(p)); // save cpsr
+            arm_write_cpsr(p, cpsr); // enter in interruptions mode
+            cpsr = set_bit(cpsr, 6); // restore cpsr config back  
+            arm_write_register(p, 15, 0x0000000C); // go to interruptions handler
+            break;
+
+        case DATA_ABORT:
+            cpsr = clr_bit(cpsr, 6); // do not disable FIQ
+            restore_pc = arm_read_register(p, 15);
+            restore_pc += 8;
+            arm_write_register(p, 14, restore_pc); // save pc
+            arm_write_spsr(p, arm_read_cpsr(p)); // save cpsr
+            arm_write_cpsr(p, cpsr); // enter in interruptions mode
+            cpsr = set_bit(cpsr, 6); // restore cpsr config back  
+            arm_write_register(p, 15, 0x00000010); // go to interruptions handler
+            break;
+
+        default:
+            // not an exception       
+            break;
+        }
     }
     return exception;
 }
