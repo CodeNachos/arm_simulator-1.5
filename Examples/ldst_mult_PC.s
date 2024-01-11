c05bc35117f9708553ca70154204da5faa4e5ae8
sanaazn <104691773+sanaazn@users.noreply.github.com>
2024-01-11 10:07:27 +0100

    Update ldst_mult_PC.s

--- Examples/ldst_mult_PC.s
+++ Examples/ldst_mult_PC.s
@@ -2,8 +2,8 @@
 .text
 main:
     // Base register addressing mode for Load Multiple (Decrement Before)
-    ldr r0, =data_section   // Load the base address of data_section into r0
-
+    ldr r0, =fin_section   // Load the base address of data_section into r0
+    sub r0,r0,#4
     // Load multiple 32-bit words with register offset (Decrement Before)
     mov r1, #4
     ldmdb r0!, {r2, r3, r4, r15}   // Load the values at the address (r0 - 12, r0 - 8, r0 - 4, r0) into r2, r3, r4, r5
@@ -35,4 +35,5 @@ data_section:
     .word 0x87654321      // 32-bit value
     .word 0xABCDEF01      // 32-bit value
     .word 0x76543210      // 32-bit value
+    .word 0x1111111F      // 32-bit value
 fin_section:
